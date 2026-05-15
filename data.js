0e00d209b9127ad72db1c501e8cdf5a6a0aa4c8c
so1dailoc <135426343+so1dailoc@users.noreply.github.com>
2026-05-16 00:14:30 +0700

    Update data.js

--- data.js
+++ data.js
@@ -8,6 +8,21 @@ window.REUNION_DATA = {
   fundGoal: 125000000, // 125 triệu
   fundRaised: 27900000, // đã gây quỹ
 
+  gallery2: [
+  { 
+    url: "images/anh1.jpg", 
+    caption: "Kỷ niệm dưới gốc phượng vĩ",
+    date: "Tháng 05/2006",
+    tag: "Kỷ niệm" 
+  },
+  { 
+    url: "images/anh2.jpg", 
+    caption: "Hội trại xuân Huỳnh Ngọc Huệ",
+    date: "Tháng 03/2005",
+    tag: "Sự kiện"
+  }
+]
+    
   schedule: [
     { time: "7:00", title: "Đón tiếp & check-in", desc: "Nhận kỷ yếu, huy hiệu, chụp ảnh lưu niệm tại khu welcome" },
     { time: "8:00", title: "Khai mạc", desc: "Phát biểu khai mạc, Văn nghệ chào mừng" },
